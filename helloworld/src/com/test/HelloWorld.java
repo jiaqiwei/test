8fc764f0ee6c14b4d8f220357bdc2f982630d19f
jiaqiwei <1426680361@qq.com>
2019-05-05 10:35:29 +0800

    no message

--- helloworld/src/com/test/HelloWorld.java
+++ helloworld/src/com/test/HelloWorld.java
@@ -6,6 +6,7 @@ package com.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("hello world");
+        System.out.print("hello world xiaowei");
     }
 }
 
